ac2dc6b60ceaca0c79f8d4fd65187f43633e3773
ydy250818 <sclsydy@sohu.com>
2025-08-23 16:14:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,14 +17,14 @@
 # git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # 设置主路由静态IP
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # 设置密码为空
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
 # 安装新主题 luci-theme-bootstrap-mod
-git clone https://github.com/ydy250818/luci-theme-bootstrap-mod.git package/lean/luci-theme-bootstrap-mod
-sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap-mod/g' feeds/luci/collections/luci/Makefile
+# git clone https://github.com/ydy250818/luci-theme-bootstrap-mod.git package/lean/luci-theme-bootstrap-mod
+# sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap-mod/g' feeds/luci/collections/luci/Makefile
 
 # 安装luci-app-smartdns和luci-app-adguardhome
 rm -rf feeds/luci/applications/luci-app-smartdns
